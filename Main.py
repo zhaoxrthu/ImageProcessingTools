37a987ef8cb77b32c4c8f1e6643b27993d9f836b
zhaoxrthu <38447581+zhaoxrthu@users.noreply.github.com>
2020-10-28 17:31:43 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,25 @@
+
+cover = """
+#############################################################
+#                 Image Processing Tools(v1.0)              #
+#                                                  by Zhaoxr#
+#Tools:                                                     #
+#   1. Movie Lines Splicing                                 #
+#   2. Scenery Mosaic                                       #
+#   3. Unzip Files                                          #
+#   4. Turn Pictures to Pdf.                                #
+#                                                           #
+#Print [1]-[4] to Choose a Function, and [q] to quit.       #
+#############################################################
+"""
+
+while True:
+    print(cover)
+    func = raw_input()
+    if func == '1':     pass
+    elif func == '2':   pass
+    elif func == '3':   pass
+    elif func == '4':   pass
+    elif func == 'q':   break
+    else:   print("Illegal Input!")
+print("Quit.")
